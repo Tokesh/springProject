6e11d00e7a52e969509f589a9ab5a5c0ef298f0d
Tokesh <tokesh789@gmail.com>
2023-04-17 02:55:19 +0600

    controller updates

--- demo/src/main/java/com/example/demo/controller/AccountController.java
+++ demo/src/main/java/com/example/demo/controller/AccountController.java
@@ -23,24 +23,14 @@ public class AccountController {
     public List<Account> findAll() {
         return accountService.findAllAccounts();
     }
-//    @PostMapping("/accounts/")
-//    public Account createAccount(@RequestBody AccountCreateDTO accountCreateDTO) throws JsonProcessingException {
+    @PostMapping("/accounts/")
+    public Account createAccount(@RequestBody AccountCreateDTO accountCreateDTO) throws JsonProcessingException {
 //        AccountCreatedEvent event = new AccountCreatedEvent();
 //        event.setEmittedDate(LocalDateTime.now());
 //        event.setAggregateObjectType("Account");
 //        event.setAggregateObject(String.valueOf(accountCreateDTO.toString()));
-//
 //        eventPublisher.publishEvent(event);
-//        System.out.println(event);
-//        return accountService.createAccount(accountCreateDTO);
-//    }
-    @PostMapping("/accounts/")
-    public void createAccount(@RequestBody AccountCreateDTO accountCreateDTO) throws JsonProcessingException {
-        AccountCreatedEvent event = new AccountCreatedEvent();
-        event.setEmittedDate(LocalDateTime.now());
-        event.setAggregateObjectType("Account");
-        event.setAggregateObject(String.valueOf(accountCreateDTO.toString()));
-        eventPublisher.publishEvent(event);
+        return accountService.createAccount(accountCreateDTO);
     }
     @PutMapping("accounts/{id}/")
     public Account updateAccount(@PathVariable Integer id,@RequestBody AccountUpdateDTO accountUpdateDTO) throws JsonProcessingException {
